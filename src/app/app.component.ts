4556758df99d9012b52d5a99dfc526fe2b3374ed
ssi-hu-szabolcs-papellas <szabolcs_papellas@surveysampling.com>
2019-04-10 09:58:51 +0200

    google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,34 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {
   NavigationEnd,
   Router
 } from '@angular/router';
-
-declare let ga: (param1, param2, param3?) => {};
+import {googleAnalytics} from './services/google.service';
+declare var gtag: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+environment = null;
+  title = 'my-app';
+
   constructor(public router: Router) {
 
-    // subscribe to router events and send page views to Google Analytics
+  }
+
+  ngAfterViewInit(): void {
     this.router.events.subscribe(event => {
 
       if (event instanceof NavigationEnd) {
-        ga('set', 'page', event.urlAfterRedirects);
-        ga('send', 'pageview');
-
+        // (window as any).ga('set', 'page', event.urlAfterRedirects);
+        // (window as any).ga('send', 'event', {name: 'pagechange'});
+        googleAnalytics(event.urlAfterRedirects);
       }
 
     });
   }
 
-  title = 'my-app';
 }
---
+++ src/app/services/google.service.ts
@@ -0,0 +1,22 @@
+declare let gtag: (param1, param2, param3) => {};
+
+export function googleAnalyticsHeadScripts() {
+    const head = document.getElementsByTagName('head')[0];
+
+    const googleAnalyticsFirstScript = document.createElement('script');
+    googleAnalyticsFirstScript.async = true;
+    googleAnalyticsFirstScript.src = 'https://www.googletagmanager.com/gtag/js?id=UA-137973739-1';
+
+    const googleAnalyticsSecondScript = document.createElement('script');
+    googleAnalyticsSecondScript.innerHTML = `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-137973739-1');`;
+
+    head.insertBefore(googleAnalyticsSecondScript, head.firstChild);
+    head.insertBefore(googleAnalyticsFirstScript, head.firstChild);
+}
+
+export function googleAnalytics(url) {
+    gtag('config', 'UA-137973739-1', { page_path: url });
+}
